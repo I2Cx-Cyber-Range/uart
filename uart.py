dd33e7a10cf8288711ef917114a7933ba9e4a2ec
Julien MOINARD <julien.moinard@lootus.net>
2020-12-15 22:29:32 +0100

    Improvements

--- uart.py
+++ uart.py
@@ -4,17 +4,26 @@ from serial import SerialException
 import threading
 import time
 
-def actions():
-    uart.write(b'commande')
+def actions1():
+    uart.write(bytes("display 20\n",'utf-8'))
 
-def boucle():
-    for i in range(0, 10):
-        uart.write(bytes("Command " + str(i),'utf-8'))
+
+def actions2():
+    data1 = bytes("AAA",'utf-8')
+    data2 = bytes([0x41,41,0x0D]) # Carriage return \n
+    uart.write(data1+data2)
+
+def loop():
+    data = bytearray()#A
+    for i in range(1, 50,1): #increment by 1
+        data.append(0x41)
+        uart.write(data + bytes([13]))
+        print("Sent: ", len(data))
         time.sleep(0.1)
 
 def searchUART():
     for port in serial.tools.list_ports.comports():
-        if( (port.vid==1155) and (port.pid==22352)):
+        if( (port.vid==1155) and (port.pid==22336)):
             return(port.device)
     return ""       
 
@@ -22,22 +31,29 @@ def ReceiveThread():
     while True:
         numBytes = uart.inWaiting()
         if numBytes > 0: 
-            print(uart.read(numBytes).decode("utf-8"))
+            try:
+                print(uart.read(numBytes).decode("utf-8"),end='')
+            except UnicodeError as decode_error:
+                next
+            
         else:
             time.sleep(0.1)
     
 def TransmitThread():
-    print("Press 1: Send a command\nPress 2: Send commands within a loop\nCTRL+C: to quit\n\n")
+    print("Press 1 or 2: Send a command\nPress 3: Send commands within a loop\nCTRL+C: to quit\n\n")
     while True:
         var = input("")
         if(var == "1"):
-            actions()
+            actions1()
         
-        elif( var == "2"):
-            boucle()
+        if(var == "2"):
+            actions2()
+        
+        elif( var == "3"):
+            loop()
 
         else:
-            print("Ce choix n'existe pas\n")
+            print("This choice does not exist\r\n")
         time.sleep(0.1)
 
 try:
@@ -48,9 +64,9 @@ try:
             threading.Thread(target=TransmitThread).start()
             threading.Thread(target=ReceiveThread).start() 
         else:
-            print("Veuillez brancher la carte I²Cx Scanner.")
+            print("Please connect I²Cx Scanner board.")
     except SerialException:
-        print("Veuillez brancher la carte I²Cx Scanner.")
+        print("Please connect I²Cx Scanner board.")
         exit()
 except (KeyboardInterrupt, SystemExit):
     exit()
